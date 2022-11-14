3b02db0f7f1013718bec76d82af87ffcebe54fc0
EN99-bit <112312714+EN99-bit@users.noreply.github.com>
2022-11-14 22:38:25 +0100

    Remove is missing

--- src/btree.c
+++ src/btree.c
@@ -6,12 +6,32 @@
 #include <stdlib.h>
 
 struct tree_node *Insert(int x, struct tree_node *t) {
+  //Rootpointer and value in the node struct
   // Insert item x into the tree t
+// 1: All children to the left of the root has to less than the value of  the root
+// 2: All children to the right of the root has to be larger than the root value
+// 3: These two rules also include the value of the childrens children
 
-  return NULL;
+if (t == NULL){ //if value is Null, create root
+struct tree_node *temp = malloc(sizeof(struct tree_node));
+temp->item = x;
+temp->left = NULL;
+temp->right = NULL;
+return temp;
 }
 
-struct tree_node *Remove(int x, struct tree_node *t) {
+if (x<=t->item){
+  t->left = Insert(x,t->left);
+  return t;
+}
+
+if (x>=t->item){
+  t->right = Insert(x,t->right);
+  return t;
+}
+}
+
+struct tree_node *Remove(int x, struct tree_node *t) { 
   // Remove one item from the tree t
 
   return NULL;
@@ -21,20 +41,38 @@ int Contains(int x, struct tree_node *t) {
 
   // Return true if the tree t contains item x. Return false otherwise.
 
-  return NULL;
+  if (t->item == NULL){
+    return 0;
+  }
+
+if (t->item == x){
+  return 1;
+}
+
+if (x<t->item){
+  return Contains(x,t->left);
+}
+
+if (x>t->item){
+  return Contains(x,t->right);
+}
+
 }
 
 struct tree_node *Initialize(struct tree_node *t) {
   // Create an empty tree
-  return NULL;
+  // Empty tree is NULL
+  t = NULL;
+  return t;
 }
 
 int Empty(struct tree_node *t) {
-  // Test if empty
-  return 0;
+  // Tree is empty when root is NULL
+  return t == NULL;
 }
 
 int Full(struct tree_node *t) {
   // Test if full
-  return 0;
+
+  return 1;
 }
